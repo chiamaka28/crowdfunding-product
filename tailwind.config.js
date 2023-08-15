f2166bc67820086fad3e7f96e15a7d001b9c87f7
chiamaka28 <chiamakamaduike@gmail.com>
2023-08-15 15:07:11 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'mobileHeader': "url('/src/assets/image-hero-mobile.jpg')",
+        'desktopHeader': "url('/src/assets/image-hero-desktop.jpg')",
+
+      },
+      colors: {
+        cyan : 'hsl(176, 50%, 47%)',
+        darkCyan: 'hsl(176, 72%, 28%)',
+        darkGray: 'hsl(0, 0%, 48%)'
+      }
+    },
+  },
+  plugins: [],
+}
+
