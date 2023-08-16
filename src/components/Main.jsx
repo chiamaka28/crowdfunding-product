b9fa5e6c9d813c8ec547578a62fb3e445dc05dcd
chiamaka28 <chiamakamaduike@gmail.com>
2023-08-16 15:27:13 +0100

    added more styling to the card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <div className="w-[90%] mx-auto bg-white shadow-md my-8 pb-6 rounded-md">
+    <div className="pb-12">
+      <div className="w-[90%] mx-auto bg-white shadow-md my-12 pb-6 rounded-md">
         <p className="flex flex-col text-center my-4">
           <span className="text-[25px] font-extrabold">$89,914</span> of
           $100,000 backed
@@ -21,7 +21,7 @@ const Main = () => {
           <div class="h-3 rounded-full bg-cyan" style={{ width: "50%" }}></div>
         </div>
       </div>
-      <div className="w-[90%] mx-auto bg-white shadow-md rounded-md text-[15px] px-4">
+      <div className="w-[90%] mx-auto bg-white shadow-md rounded-md text-[15px] px-4 pb-9">
         <h3 className="font-bold text-[17px]">About this project</h3>
         <div className="text-darkGray">
           <p>
@@ -37,40 +37,42 @@ const Main = () => {
             USB sticks to be stored under the stand.
           </p>
         </div>
-        <div className="border border-darkGray ">
-          <h4>Bamboo Stand</h4>
-          <p className="text-cyan">Pledge $25 or more</p>
-          <p className="text-darkGray">
-            {" "}
-            You get an ergonomic stand made of natural bamboo. You've helped us
-            launch our promotional campaign, and you’ll be added to a special
-            Backer member list.
-          </p>
-          <p>101 left</p>
-          <button> Select Reward</button>
-        </div>
-        <div className="border border-darkGray ">
-          <h4>Black Edition Stand</h4>
-          <p className="text-cyan">Pledge $75 or more</p>
-          <p className="text-darkGray">
-            {" "}
-            You get a Black Special Edition computer stand and a personal thank
-            you. You’ll be added to our Backer member list. Shipping is
-            included.
-          </p>
-          <p>64 left</p>
-          <button> Select Reward</button>
-        </div>
-        <div className="border border-darkGray ">
-          <h4>Mahogany Special Edition Pledge</h4>
-          <p className="text-cyan"> $200 or more</p> 
-          <p className="text-darkGray">
-            You get two Special
-            Edition Mahogany stands, a Backer T-Shirt, and a personal thank you.
-            You’ll be added to our Backer member list. Shipping is included.
-          </p> 
-          <p>0left</p> 
-          <button>Out of Stock</button>
+        <div className="flex flex-col gap-y-5 mt-6">
+          <div className="border border-darkGray py-6 px-4 rounded-md">
+            <h4 className="font-bold">Bamboo Stand</h4>
+            <p className="text-cyan">Pledge $25 or more</p>
+            <p className="text-darkGray py-5 ">
+              {" "}
+              You get an ergonomic stand made of natural bamboo. You've helped us
+              launch our promotional campaign, and you’ll be added to a special
+              Backer member list.
+            </p>
+            <p><span className="text-[26px] font-extrabold">101</span> left</p>
+            <button className="bg-cyan py-2 px-4 text-white rounded-3xl my-2"> Select Reward</button>
+          </div>
+          <div className="border border-darkGray py-6 px-4 rounded-md">
+            <h4 className="font-bold">Black Edition Stand</h4>
+            <p className="text-cyan">Pledge $75 or more</p>
+            <p className="text-darkGray py-5">
+              {" "}
+              You get a Black Special Edition computer stand and a personal thank
+              you. You’ll be added to our Backer member list. Shipping is
+              included.
+            </p>
+            <p><span className="text-[26px] font-extrabold">64</span> left</p>
+            <button className="bg-cyan py-2 px-4  text-white rounded-3xl my-2"> Select Reward</button>
+          </div>
+          <div className="border border-darkGray py-6 px-4 rounded-md">
+            <h4 className="font-bold">Mahogany Special Edition Pledge</h4>
+            <p className="text-cyan"> $200 or more</p>
+            <p className="text-darkGray py-5">
+              You get two Special
+              Edition Mahogany stands, a Backer T-Shirt, and a personal thank you.
+              You’ll be added to our Backer member list. Shipping is included.
+            </p>
+            <p><span className="text-[26px] font-extrabold">0</span>left</p>
+            <button className="bg-cyan py-2 px-4  text-white rounded-3xl my-2">Out of Stock</button>
+          </div>
         </div>
       </div>
     </div>
