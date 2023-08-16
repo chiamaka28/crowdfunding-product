f866f56d21d233401a4d323a76246ef3614cd4df
chiamaka28 <chiamakamaduike@gmail.com>
2023-08-16 14:22:21 +0100

    added some  styles to the cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,22 +23,24 @@ const Main = () => {
       </div>
       <div className="w-[90%] mx-auto bg-white shadow-md rounded-md text-[15px] px-4">
         <h3 className="font-bold text-[17px]">About this project</h3>
-        <p>
-          The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform
-          that elevates your screen to a more comfortable viewing height.
-          Placing your monitor at eye level has the potential to improve your
-          posture and make you more comfortable while at work, helping you stay
-          focused on the task at hand.
-        </p>
-        <p className="pt-4">
-          Featuring artisan craftsmanship, the simplicity of design creates
-          extra desk space below your computer to allow notepads, pens, and USB
-          sticks to be stored under the stand.
-        </p>
-        <div>
-          <h4>Bamboo Stand</h4>
-          <p>Pledge $25 or more</p>
+        <div className="text-darkGray">
           <p>
+            The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
+            platform that elevates your screen to a more comfortable viewing
+            height. Placing your monitor at eye level has the potential to
+            improve your posture and make you more comfortable while at work,
+            helping you stay focused on the task at hand.
+          </p>
+          <p className="pt-4">
+            Featuring artisan craftsmanship, the simplicity of design creates
+            extra desk space below your computer to allow notepads, pens, and
+            USB sticks to be stored under the stand.
+          </p>
+        </div>
+        <div className="border border-darkGray ">
+          <h4>Bamboo Stand</h4>
+          <p className="text-cyan">Pledge $25 or more</p>
+          <p className="text-darkGray">
             {" "}
             You get an ergonomic stand made of natural bamboo. You've helped us
             launch our promotional campaign, and you’ll be added to a special
@@ -47,10 +49,10 @@ const Main = () => {
           <p>101 left</p>
           <button> Select Reward</button>
         </div>
-        <div>
+        <div className="border border-darkGray ">
           <h4>Black Edition Stand</h4>
-          <p>Pledge $75 or more</p>
-          <p>
+          <p className="text-cyan">Pledge $75 or more</p>
+          <p className="text-darkGray">
             {" "}
             You get a Black Special Edition computer stand and a personal thank
             you. You’ll be added to our Backer member list. Shipping is
@@ -58,6 +60,17 @@ const Main = () => {
           </p>
           <p>64 left</p>
           <button> Select Reward</button>
+        </div>
+        <div className="border border-darkGray ">
+          <h4>Mahogany Special Edition Pledge</h4>
+          <p className="text-cyan"> $200 or more</p> 
+          <p className="text-darkGray">
+            You get two Special
+            Edition Mahogany stands, a Backer T-Shirt, and a personal thank you.
+            You’ll be added to our Backer member list. Shipping is included.
+          </p> 
+          <p>0left</p> 
+          <button>Out of Stock</button>
         </div>
       </div>
     </div>
